df18a39e0eb5579099c2c80873fb8bc69b357661
Love4653 <league@WTS11.attlocal.net>
2016-07-01 14:56:30 -0700

    hi

--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -17,33 +17,39 @@ public class FlamingNinjaStar {
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 1. Make a new robot, and set it's pen down.
-
+Robot meme = new Robot();
+meme.penDown();
 		// 12. Set the robot speed to 10
-
+meme.setSpeed(10);
 		// 13. Make all the code below repeat 25 times
+for (int i = 0; i < 25; i++) {
+	
 
 			// 2. Turn the robot 1/8 of a circle
-		
+		meme.turn(45);
 			// 3. Move the robot 64 pixels
-
-			
+meme.move(64);
+			meme.setPenColor(255,0,0);
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+	meme.turn(-40);		
 			// 5. Move the robot the distance in the variable flameSize
-			
+			meme.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+			meme.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			meme.move(flameSize);
+			meme.setPenColor(0,0,0);
 			// 8. Turn the robot 64 degrees to the right
-			
+			meme.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
+			meme.setPenColor(0,0,0);
+			meme.move(baseSize);
+			meme.setPenColor(255,0,0);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			// 11. Color your ninja star like Figure 2.
 		
 	}
-
+	}
 }
 
 
